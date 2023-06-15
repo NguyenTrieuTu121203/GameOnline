66c5063604a71aa31dfad72297efe5d1ee1dd3f1
NguyenTrieuTu121203 <21522743@gn.uit.edu.vn>
2023-06-15 19:22:30 +0700

    Server

---
+++ Server/Server/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.DebugTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.databaseDataSet = new Server.DatabaseDataSet();
+            this.usersTableAdapter = new Server.DatabaseDataSetTableAdapters.UsersTableAdapter();
+            this.fillBy2ToolStrip = new System.Windows.Forms.ToolStrip();
+            this.fillBy2ToolStripButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).BeginInit();
+            this.fillBy2ToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // DebugTextBox
+            // 
+            this.DebugTextBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.DebugTextBox.Location = new System.Drawing.Point(17, 76);
+            this.DebugTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.DebugTextBox.Multiline = true;
+            this.DebugTextBox.Name = "DebugTextBox";
+            this.DebugTextBox.ReadOnly = true;
+            this.DebugTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.DebugTextBox.Size = new System.Drawing.Size(723, 290);
+            this.DebugTextBox.TabIndex = 0;
+            this.DebugTextBox.Text = "Server application initialized";
+            this.DebugTextBox.TextChanged += new System.EventHandler(this.DebugTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.usersBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(779, 76);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(619, 290);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // passwordDataGridViewTextBoxColumn
+            // 
+            this.passwordDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
+            this.passwordDataGridViewTextBoxColumn.HeaderText = "Password";
+            this.passwordDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataMember = "Users";
+            this.usersBindingSource.DataSource = this.databaseDataSet;
+            // 
+            // databaseDataSet
+            // 
+            this.databaseDataSet.DataSetName = "DatabaseDataSet";
+            this.databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // usersTableAdapter
+            // 
+            this.usersTableAdapter.ClearBeforeFill = true;
+            // 
+            // fillBy2ToolStrip
+            // 
+            this.fillBy2ToolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.fillBy2ToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fillBy2ToolStripButton});
+            this.fillBy2ToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.fillBy2ToolStrip.Name = "fillBy2ToolStrip";
+            this.fillBy2ToolStrip.Size = new System.Drawing.Size(1418, 27);
+            this.fillBy2ToolStrip.TabIndex = 5;
+            this.fillBy2ToolStrip.Text = "fillBy2ToolStrip";
+            // 
+            // fillBy2ToolStripButton
+            // 
+            this.fillBy2ToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillBy2ToolStripButton.Name = "fillBy2ToolStripButton";
+            this.fillBy2ToolStripButton.Size = new System.Drawing.Size(56, 24);
+            this.fillBy2ToolStripButton.Text = "FillBy2";
+            this.fillBy2ToolStripButton.Click += new System.EventHandler(this.fillBy2ToolStripButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1418, 386);
+            this.Controls.Add(this.fillBy2ToolStrip);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DebugTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet)).EndInit();
+            this.fillBy2ToolStrip.ResumeLayout(false);
+            this.fillBy2ToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.TextBox DebugTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DatabaseDataSet databaseDataSet;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+        public DatabaseDataSetTableAdapters.UsersTableAdapter usersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ToolStrip fillBy2ToolStrip;
+        private System.Windows.Forms.ToolStripButton fillBy2ToolStripButton;
+    }
+}
+
---
+++ Server/Server/Program.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+
+namespace Server
+{
+    static class Program
+    {
+        public static ServerAction server;
+        public static Form1 form;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //use points for floats for easy compatibility with coordinates
+            CultureInfo customCulture = (CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            server = new ServerAction();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form = new Form1();
+            Application.Run(form);
+
+        }
+
+    }
+
+    public class ServerAction
+    {
+        private int port = 6321;
+
+        private List<ServerClient> clients = new List<ServerClient>();
+        private List<ServerClient> disconnectList = new List<ServerClient>();
+        private TcpListener server;
+        private bool serverStarted;
+        private List<Unit> units = new List<Unit>();
+        private bool ResyncNeeded = false;
+
+        //the constructor, adds the listener
+        public ServerAction()
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Any, port);
+                server.Start();
+                StartListening();
+                serverStarted = true;
+            }
+            catch (Exception e)
+            {
+                Program.form.DebugTextBox.Text += "\r\n" + e.Message;
+            }
+        }
+      
+
+        //called at every fixed time intervals => time can be adjusted at timer component's property
+        //used to check if there's incoming data
+        public void Update()
+        {
+            if (!serverStarted)
+                return;
+
+            foreach (ServerClient c in clients)
+            {
+                // Is the client still connected?
+                if (!IsConnected(c.tcp))
+                {
+                    c.tcp.Close();
+                    disconnectList.Add(c);
+                    continue;
+                }
+                else
+                {
+                    NetworkStream s = c.tcp.GetStream();
+                    if (s.DataAvailable)
+                    {
+                        StreamReader reader = new StreamReader(s, true);
+                        string data = reader.ReadLine();
+
+                        if (data != null)
+                            OnIncomingData(c, data);
+                    }
+                }
+            }
+
+            //checking disconnected players
+            for (int i = 0; i < disconnectList.Count - 1; i++)
+            {
+                for (int j = 0; j < units.Count; j++)
+                {
+                    if (units[j].clientName == disconnectList[i].clientName)
+                    {
+                        units.RemoveAt(j);
+                    }
+                }
+                Program.form.DebugTextBox.Text += "\r\nUser disconnected:" + disconnectList[i].clientName;
+                clients.Remove(disconnectList[i]);
+                disconnectList.RemoveAt(i);
+                ResyncNeeded = true;
+            }
+            //if some1 disconnected, tell it to other players as well.
+            if (ResyncNeeded){
+                SynchronizeUnits();
+                ResyncNeeded = false;
+            }
+        }
+
+        private void StartListening()
+        {
+            server.BeginAcceptTcpClient(AcceptTcpClient, server);
+        }
+
+        private void AcceptTcpClient(IAsyncResult ar)
+        {
+            TcpListener listener = (TcpListener)ar.AsyncState;
+
+            string allUsers = "";
+            foreach (ServerClient i in clients)
+            {
+                allUsers += i.clientName + '|';
+            }
+
+            ServerClient sc = new ServerClient(listener.EndAcceptTcpClient(ar));
+            clients.Add(sc);
+
+            StartListening();
+            //request authentication from client
+            Broadcast("WhoAreYou|", clients[clients.Count - 1]);
+        }
+
+        private bool IsConnected(TcpClient c)
+        {
+            try
+            {
+                if (c != null && c.Client != null && c.Client.Connected)
+                {
+                    if (c.Client.Poll(0, SelectMode.SelectRead))
+                        return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        // Server Send
+        private void Broadcast(string data, List<ServerClient> cl)
+        {
+            foreach (ServerClient sc in cl)
+            {
+                try
+                {
+                    StreamWriter writer = new StreamWriter(sc.tcp.GetStream());
+                    writer.WriteLine(data);
+                    writer.Flush();
+                }
+                catch (Exception e)
+                {
+                    Program.form.DebugTextBox.Text += "\r\n" + e.Message;
+                }
+            }
+        }
+
+        private void Broadcast(string data, ServerClient c)
+        {
+            List<ServerClient> sc = new List<ServerClient> { c };
+            Broadcast(data, sc);
+        }
+
+        // Server Read
+        private void OnIncomingData(ServerClient c, string data)
+        {
+            string[] aData = data.Split('|');
+
+            //login
+            if (c.clientName != null)
+            {
+                Program.form.DebugTextBox.Text += "\r\nClient '" + c.clientName + "' sent command: " + data;
+            }
+            else
+            {
+                Program.form.DebugTextBox.Text += "\r\nNew Client trying to join server. Requesting authentication.";
+                if (aData[0] == "Iam")
+                {
+                    bool authenticated =  Database.AuthenticateUser(aData[1], aData[2]);
+                    if (authenticated)
+                    {
+                        foreach (ServerClient client in clients)
+                        {
+                            if (aData[1] == client.clientName)
+                            {
+                                Program.form.DebugTextBox.Text += "\r\nThis user is already connected";
+                                c.tcp.Close();
+                                disconnectList.Add(c);
+                                return;
+                            }
+                        }
+                        c.clientName = aData[1];
+                        Program.form.DebugTextBox.Text += "\r\nUser authenticated";
+                        Broadcast("Authenticated|", c);
+                    }
+                    else
+                    {
+                        Program.form.DebugTextBox.Text += "\r\nUser authentication failed, client disconnected.";
+                        c.tcp.Close();
+                        disconnectList.Add(c);
+                    }
+                    return; 
+                }
+                
+            }
+
+
+            //gameplay commands
+            switch (aData[0])
+            {
+                case "SynchronizeRequest":
+                    SynchronizeUnits(c);
+                    break;
+                case "SpawnUnit":
+                    Unit unit = new Unit();
+                    unit.clientName = c.clientName;
+                    //give a new ID to the new units
+                    int newid = 0;
+                    foreach (Unit u in units)
+                    {
+                        if (u.unitID >= newid) { newid = u.unitID + 1; }
+                    }
+
+                    unit.unitID = newid;
+                    unit.unitPositionX = 0.1f;
+                    unit.unitPositionY = -6.09f;
+                    unit.unitPositionZ = 0.0f;
+                    units.Add(unit);
+                    
+                    Broadcast("UnitSpawned|" + c.clientName + "|" + unit.unitID + "|" + unit.unitPositionX + "|" + unit.unitPositionY + "|" + unit.unitPositionZ, clients);
+                    break;
+                case "Moving":
+                    Broadcast("UnitMoved|" + c.clientName + "|" + aData[1] + "|" + aData[2] + "|" + aData[3] + "|" + aData[4] + "|" + aData[5], clients);
+                    int id;
+                    Int32.TryParse(aData[1], out id);
+                    float parsedX;
+                    float parsedY;
+                    int AnimtionState;
+                    int ValueFlip;
+                    float.TryParse(aData[2], out parsedX);
+                    float.TryParse(aData[3], out parsedY);
+                    Int32.TryParse(aData[4], out AnimtionState);
+                    Int32.TryParse(aData[5], out ValueFlip);
+                    foreach (Unit u in units)
+                    {
+                        if (u.unitID == id)
+                        {
+                            u.unitPositionX = parsedX;
+                            u.unitPositionY = parsedY;
+                            u.ValueStateAnim = AnimtionState;
+                            u.IsFlipRight = ValueFlip;
+                        }
+                    }
+                    Program.form.DebugTextBox.Text += "\r\n" + parsedX + "  " + parsedY + "  " + AnimtionState;
+                    break;
+                case "Chat":
+                    Program.form.DebugTextBox.Text += "\r\nChat";
+                    break;
+                default:
+                    Program.form.DebugTextBox.Text += "\r\nReceived unknown signal => skipping";
+                    break;
+            }
+        }
+        
+        //syncing 1 client
+        private void SynchronizeUnits(ServerClient c)
+        {
+            string dataToSend = "Synchronizing|" + units.Count;
+            /*string dataToSendFlip = "SynchronizingFlip|" + units.Count;*/
+            
+            foreach (Unit u in units)
+            {
+                dataToSend += "|" + (u.unitID) + "|" + u.unitPositionX + "|" + u.unitPositionY + "|" + u.ValueStateAnim + "|" +u.IsFlipRight;
+                /*dataToSendFlip += "|" + (u.unitID) + "|" + u.IsFlipRight;*/
+                
+            }
+            
+            Broadcast(dataToSend, c);
+            /*Broadcast(dataToSendFlip, c);*/
+            
+            Program.form.DebugTextBox.Text += "\r\nSynchronization request sent: " + dataToSend;
+           /* Program.form.DebugTextBox.Text += "\r\nSynchronization request sent FLIP: " + dataToSendFlip;*/
+            
+        }
+
+        //syncing all clients
+        private void SynchronizeUnits()
+        {
+            string dataToSend = "Synchronizing|" + units.Count;
+            /*string dataToSendFlip = "SynchronizingFlip|" + units.Count;*/
+            
+            foreach (Unit u in units)
+            {
+                dataToSend += "|" + (u.unitID) + "|" + u.unitPositionX + "|" + u.unitPositionY + "|" + u.ValueStateAnim + "|" +u.IsFlipRight;
+               /* dataToSendFlip += "|" + (u.unitID) + "|" + u.IsFlipRight;*/
+                
+            }
+            
+            Broadcast(dataToSend, clients);
+            /*Broadcast(dataToSendFlip, clients);*/
+           
+            Program.form.DebugTextBox.Text += "\r\nSynchronization request sent: " + dataToSend;
+           /* Program.form.DebugTextBox.Text += "\r\nSynchronization request sent FLIP: " + dataToSendFlip;*/
+            
+        }
+    }
+    
+
+    public class ServerClient
+    {
+        public string clientName;
+        public TcpClient tcp;
+        public ServerClient(TcpClient tcp)
+
+        {
+            this.tcp = tcp;
+        }
+    }
+
+    public class Unit
+    {
+        public string clientName;
+        public int unitID;
+        public float unitPositionX;
+        public float unitPositionY;
+        public float unitPositionZ;
+        public int IsFlipRight;
+        public int ValueStateAnim;
+    }
+
+    public static class HashMD5
+    {
+        public static string GetMd5Hash(string input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                // Chuyển đổi chuỗi đầu vào thành một mảng byte và tính toán mã hash
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Tạo một StringBuilder để chứa các byte đã tính toán thành mã hash
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Lặp qua mỗi byte trong mảng và định dạng chúng thành một chuỗi hex
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Trả về chuỗi hex đã được tạo thành từ mã hash
+                return sBuilder.ToString();
+            }
+        }
+    }
+
+
+    public static class Database
+    {
+        public static bool AuthenticateUser(string username, string password)
+        {
+            int result;
+            string PassAfterHash;
+            PassAfterHash = HashMD5.GetMd5Hash(password);
+            result = (int)Program.form.usersTableAdapter.Authenticate(username, PassAfterHash);
+            if (result == 1)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
